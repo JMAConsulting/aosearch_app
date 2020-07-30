18dc908a0dbfaf5ff871d5f9fe4785ed99993861
Cali0707 <65566908+Cali0707@users.noreply.github.com>
2020-07-30 16:09:41 -0400

    Added multiple dropdown menus

--- lib/src/ui/advanced_search_form.dart
+++ lib/src/ui/advanced_search_form.dart
@@ -11,6 +11,7 @@ class _AdvancedSearchFormState extends State<AdvancedSearchForm> {
 
   final _formKey = GlobalKey<FormState>();
   String languages = 'English';
+  String acceptingNewClients = 'Any';
 
   @override
   Widget build(BuildContext context) {
@@ -19,43 +20,88 @@ class _AdvancedSearchFormState extends State<AdvancedSearchForm> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          ListTile(
-            title: TextFormField(
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-              decoration: InputDecoration(
-                hintText: 'Enter your search term here',
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Icon(Icons.search),
-                )
+          Expanded(
+            child: ListTile(
+              title: TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  hintText: 'Enter your search term here',
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(Icons.search),
+                  )
+                ),
               ),
             ),
           ),
-          Card(
-            margin: EdgeInsets.all(30.0),
-            child: DropdownButtonFormField<String>(
-              value: languages,
-              icon: Icon(Icons.keyboard_arrow_down),
-              onChanged: (String newValue) {
-                setState(() {
-                  languages = newValue;
-                });
-              },
-              items: <String>[
-                'English',
-                'French'
-              ].map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all()
+                  ),
+                    child: DropdownButtonHideUnderline(
+                      child: DropdownButtonFormField<String>(
+                        value: languages,
+                        icon: Icon(Icons.keyboard_arrow_down),
+                        onChanged: (String newValue) {
+                          setState(() {
+                            languages = newValue;
+                          });
+                        },
+                        items: <String>[
+                          'English',
+                          'French'
+                        ].map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  ),
+                ),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(10.0),
+                  padding: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    border: Border.all()
+                  ),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButtonFormField(
+                      value: acceptingNewClients,
+                      icon: Icon(Icons.keyboard_arrow_down),
+                      onChanged: (String newValue) {
+                        setState(() {
+                          acceptingNewClients = newValue;
+                        });
+                      },
+                      items: <String>[
+                        'Any',
+                        'Yes',
+                        'No'
+                      ].map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ),
+              )
+            ],
           ),
           RaisedButton(
             onPressed: () {
